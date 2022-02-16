f6020c3c471747a03e79d5c057cd91c743e1ddc5
George <georgesucuzhanay100@gmail.com>
2022-02-15 22:07:24 -0500

    completed routing blog lab

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import './App.css';
+import { NavLink, Route, Routes } from 'react-router-dom';
+import data from './FakeData.json';
+import Home from './screens/Home';
+import Posts from './screens/Posts';
+import About from './screens/About';
+import Post from './screens/Post';
+import NoMatch from './screens/NoMatch';
+
+// NavLink:  is a component for setting the URL and providing
+// navigation between components in our app
+// without triggering a page refresh
+
+// we can even style in the NavLink itself
+// that links paths to components and they link to each other
+
+// Route: a component that connects a certain path
+// in the URL with the relevant component to render
+// at that location
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className='links'>
+        <nav>
+          <NavLink to={"/"}>Home</NavLink>
+          <NavLink to={"/about"}>About</NavLink>
+          <NavLink to={"/posts"}>Posts</NavLink>
+        </nav>
+      </div>
+
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/posts" element={<Posts posts={data}/>}/>
+        <Route path="/posts/:post_id" element={<Post {...data}/>}/>
+        <Route path="*" element={<NoMatch/>}/>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
